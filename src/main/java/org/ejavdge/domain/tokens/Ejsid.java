46eac5b9c98bed38dc4d2a716dfe611ce374178b
F1rrock <flyrock000@gmail.com>
2026-08-02 22:58:26 +0300

    feature: add Ejsid and Sid tokens

--- src/main/java/org/ejavdge/domain/tokens/Ejsid.java
+++ src/main/java/org/ejavdge/domain/tokens/Ejsid.java
@@ -1,4 +1,34 @@
 package org.ejavdge.domain.tokens;
 
-public class Ejsid {
+import org.ejavdge.auth.LoginReply;
+import org.ejavdge.error.InvariantViolation;
+import org.ejavdge.scalar.text.Match;
+import org.ejavdge.scalar.text.Text;
+import org.ejavdge.scalar.text.Utf8Text;
+import org.ejavdge.web.context.Credentials;
+import org.ejavdge.web.context.Location;
+import org.ejavdge.web.driver.WebDriver;
+
+public final class Ejsid implements Text {
+    private final Text origin;
+
+    public Ejsid(final WebDriver d, final Location l, final Credentials c) {
+        this(
+            new Match(
+                new Utf8Text(
+                    new LoginReply(d, l, c)
+                ),
+                new Text.Of("(?<=EJSID=)[^;]+")
+            )
+        );
+    }
+
+    public Ejsid(final Text t) {
+        this.origin = t;
+    }
+
+    @Override
+    public String content() throws InvariantViolation {
+        return this.origin.content();
+    }
 }
--- src/main/java/org/ejavdge/domain/tokens/Sid.java
+++ src/main/java/org/ejavdge/domain/tokens/Sid.java
@@ -1,4 +1,34 @@
 package org.ejavdge.domain.tokens;
 
-public class Sid {
+import org.ejavdge.auth.LoginReply;
+import org.ejavdge.error.InvariantViolation;
+import org.ejavdge.scalar.text.Match;
+import org.ejavdge.scalar.text.Text;
+import org.ejavdge.scalar.text.Utf8Text;
+import org.ejavdge.web.context.Credentials;
+import org.ejavdge.web.context.Location;
+import org.ejavdge.web.driver.WebDriver;
+
+public final class Sid implements Text {
+    private final Text origin;
+
+    public Sid(final WebDriver d, final Location l, final Credentials c) {
+        this(
+            new Match(
+                new Utf8Text(
+                    new LoginReply(d, l, c)
+                ),
+                new Text.Of("(?<=[?&]SID=)[^&]+")
+            )
+        );
+    }
+
+    public Sid(final Text t) {
+        this.origin = t;
+    }
+
+    @Override
+    public String content() throws InvariantViolation {
+        return this.origin.content();
+    }
 }

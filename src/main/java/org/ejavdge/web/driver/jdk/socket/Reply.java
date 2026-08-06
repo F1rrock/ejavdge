ebda5fb1c7aeebf4a1d22e106ebcf787b6b89c02
F1rrock <flyrock000@gmail.com>
2026-08-06 14:48:08 +0300

    feat: add Reply for raw socket communication

---
+++ src/main/java/org/ejavdge/web/driver/jdk/socket/Reply.java
@@ -0,0 +1,40 @@
+package org.ejavdge.web.driver.jdk.socket;
+
+import org.ejavdge.error.InvariantViolation;
+import org.ejavdge.web.context.Location;
+import org.ejavdge.web.spec.Request;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public final class Reply {
+    private final Location loc;
+    private final Request req;
+
+    public Reply(final Location l, final Request r) {
+        this.loc = l;
+        this.req = r;
+    }
+
+    public InputStream stream() throws InvariantViolation {
+        var socket = new Inet(this.loc).socket();
+        try {
+            var out = socket.getOutputStream();
+            out.write(this.req.bytes());
+            out.flush();
+            return socket.getInputStream();
+        } catch (final IOException e) {
+            try {
+                socket.close();
+            } catch (final IOException ex) {
+                e.addSuppressed(ex);
+                throw new InvariantViolation(
+                    "There is no socket.\n" + e.getMessage()
+                );
+            }
+            throw new InvariantViolation(
+                "There is no valid resource.\n" + e.getMessage()
+            );
+        }
+    }
+}

80468206bdba32a52de7310db98fedfa470baaa2
F1rrock <flyrock000@gmail.com>
2026-08-08 12:17:19 +0300

    feat: app with contest page example has been added

--- src/main/java/org/ejavdge/App.java
+++ src/main/java/org/ejavdge/App.java
@@ -1,36 +1,37 @@
 package org.ejavdge;
 
-import org.ejavdge.auth.LoginReply;
+import org.ejavdge.auth.Session;
+import org.ejavdge.page.ContestPage;
 import org.ejavdge.scalar.num.NumOfText;
 import org.ejavdge.scalar.text.Text;
 import org.ejavdge.web.context.Credentials;
 import org.ejavdge.web.context.Location;
 import org.ejavdge.web.driver.jdk.socket.JdkSocket;
 
-import java.nio.charset.StandardCharsets;
-
 @SuppressWarnings("java:S106")
 public final class App
 {
     public static void main(final String[] args)
     {
+        final var loc = new Location(
+            new Text.Of(args[0]),
+            new Text.Of(args[1]),
+            new NumOfText(args[2])
+        );
         System.out.println(
-            new String(
-                new LoginReply(
+            new ContestPage(
+                new JdkSocket(),
+                loc,
+                new Session(
                     new JdkSocket(),
-                    new Location(
-                        new Text.Of(args[0]),
-                        new Text.Of(args[1]),
-                        new NumOfText(args[2])
-                    ),
+                    loc,
                     new Credentials(
                         new Text.Of(args[3]),
                         new Text.Of(args[4]),
                         new NumOfText(args[5])
                     )
-                ).content(),
-                StandardCharsets.UTF_8
-            )
+                )
+            ).content()
         );
     }
 }

1a2dd388f30059e9014c902646557f1707a37bde
F1rrock <flyrock000@gmail.com>
2026-08-22 22:32:14 +0300

    feat: add constructor of ByteFile

--- src/test/java/org/ejavdge/web/spec/body/multipart/FilePartTest.java
+++ src/test/java/org/ejavdge/web/spec/body/multipart/FilePartTest.java
@@ -2,6 +2,7 @@ package org.ejavdge.web.spec.body.multipart;
 
 import junit.framework.TestCase;
 import org.ejavdge.error.InvariantViolation;
+import org.ejavdge.file.ByteFile;
 import org.ejavdge.scalar.bytes.Bytes;
 import org.ejavdge.scalar.text.Empty;
 import org.ejavdge.scalar.text.Text;
@@ -49,6 +50,25 @@ public final class FilePartTest extends TestCase {
                 new FilePart(
                     new Text.Of("Empty.java"),
                     new Bytes.Of(new byte[0])
+                ).content(),
+                StandardCharsets.UTF_8
+            )
+        );
+    }
+
+    public void testWithFile() {
+        assertEquals(
+            """
+            Content-Disposition: form-data; name="file"; filename="hello.py"\r
+            Content-Type: application/octet-stream\r
+            \r
+            Hello, world!""",
+            new String(
+                new FilePart(
+                    new ByteFile.Of(
+                        new Text.Of("hello.py"),
+                        new Bytes.Of("Hello, world!".getBytes(StandardCharsets.UTF_8))
+                    )
                 ).content(),
                 StandardCharsets.UTF_8
             )

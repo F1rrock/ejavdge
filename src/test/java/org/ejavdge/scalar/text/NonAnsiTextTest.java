ff063e74eacbc319798bf1d7d3809153d5c4ffe7
F1rrock <flyrock000@gmail.com>
2026-09-13 14:32:53 +0800

    feat: add non-ansi text

---
+++ src/test/java/org/ejavdge/scalar/text/NonAnsiTextTest.java
@@ -0,0 +1,62 @@
+package org.ejavdge.scalar.text;
+
+import junit.framework.TestCase;
+import org.ejavdge.error.InvariantViolation;
+
+public final class NonAnsiTextTest extends TestCase {
+    public void testPlainText() {
+        assertEquals(
+            "hello world",
+            new NonAnsiText(
+                new Text.Of("hello world")
+            ).content()
+        );
+    }
+
+    public void testRedText() {
+        assertEquals(
+            "hello",
+            new NonAnsiText(
+                new Text.Of("\u001B[31mhello\u001B[0m")
+            ).content()
+        );
+    }
+
+    public void testSeveralAnsiStylesAreRemoved() {
+        assertEquals(
+            "hello",
+            new NonAnsiText(
+                new Text.Of("\u001B[1;31mhello\u001B[0m")
+            ).content()
+        );
+    }
+
+    public void testSeveralAnsiCodes() {
+        assertEquals(
+            "hello world",
+            new NonAnsiText(
+                new Text.Of("hello \u001B[31mworld\u001B[0m")
+            ).content()
+        );
+    }
+
+    public void testAnsiControlSequence() {
+        assertEquals(
+            "hello world",
+            new NonAnsiText(
+                new Text.Of("hello \u001B[2Jworld")
+            ).content()
+        );
+    }
+
+    public void testBrokenOrigin() {
+        try {
+            new NonAnsiText(() -> {
+                throw new InvariantViolation("there is no text");
+            }).content();
+        } catch (final InvariantViolation err) {
+            return;
+        }
+        fail("InvariantViolation");
+    }
+}

39d5dc3d7157044144114a37957ff4e1a9b321f7
F1rrock <flyrock000@gmail.com>
2026-09-17 14:03:29 +0800

    chore: rename WithLogging to WithLogsDriver and wrap request bytes to memo decorator

--- src/main/java/org/ejavdge/web/driver/WithLogsDriver.java
+++ src/main/java/org/ejavdge/web/driver/WithLogsDriver.java
@@ -1,32 +1,30 @@
 package org.ejavdge.web.driver;
 
+import org.ejavdge.scalar.bytes.Memo;
+import org.ejavdge.web.spec.ByteView;
 import org.slf4j.Logger;
 
-import org.ejavdge.scalar.bytes.Bytes;
 import org.ejavdge.scalar.text.Utf8Text;
 import org.ejavdge.web.context.Location;
 import org.ejavdge.web.spec.Request;
 
-public final class WithLogging implements WebDriver {
+public final class WithLogsDriver implements WebDriver {
     private final WebDriver origin;
     private final Logger log;
 
-    public WithLogging(final WebDriver d, final Logger l) {
+    public WithLogsDriver(final WebDriver d, final Logger l) {
         this.origin = d;
         this.log = l;
     }
 
     @Override
     public byte[] resourceOf(final Location loc, final Request req) {
+        final var bs = new Memo(new ByteView(req));
         if (log.isTraceEnabled()) {
             this.log.trace(
-                new Utf8Text(
-                    new Bytes.Of(
-                        req.bytes()
-                    )
-                ).content()
+                new Utf8Text(bs).content()
             );
         }
-        return this.origin.resourceOf(loc, req);
+        return this.origin.resourceOf(loc, new Request(bs));
     }
 }

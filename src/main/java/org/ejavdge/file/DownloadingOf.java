be9f4891978ec0646db61ea00a65407c5fc7ddd6
F1rrock <flyrock000@gmail.com>
2026-09-26 10:33:17 +0800

    style: add a space after us parameter

--- src/main/java/org/ejavdge/file/DownloadingOf.java
+++ src/main/java/org/ejavdge/file/DownloadingOf.java
@@ -22,7 +22,7 @@ public final class DownloadingOf implements Effect {
     private final Text path;
     private final Items<ByteFile> files;
 
-    public DownloadingOf(final Items<Text> us,final WebDriver d, final Text p) {
+    public DownloadingOf(final Items<Text> us, final WebDriver d, final Text p) {
         this.path = p;
         this.files = new Map<>(
             u -> new ByteFile.Of(
